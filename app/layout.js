ae3ff3ccc1b1dbcf8cb5b5df9f50a394c347f039
chucoyos <unodev@outlook.com>
2025-11-15 03:05:32 -0600

    Replace favicon.ico

--- app/layout.js
+++ app/layout.js
@@ -15,11 +15,17 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "IPBroker - Despacho Aduanal",
   description: "Soluciones integrales para tus operaciones de comercio internacional",
+  icons: {
+    icon: '/images/favicon.png',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="/images/favicon.png" type="image/png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
